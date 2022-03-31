6c288d792e9ce7c5c3b6b608149f733457fa3e4b
DavidVista <ilyagrigorev0505@gmail.com>
2022-03-31 16:36:41 +0500

    created text-model

--- server/reapp/models.py
+++ server/reapp/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
-class Product(models.Model):
+class Text(models.Model):
+    THEME = (
+    ('Light', 'Light'),
+    ('Dark', 'Dark'),
+    ('Monokai', 'Monokai'),
+    ('Chocolate', 'Chocolate')
+    )
+
     name = models.CharField(max_length=255) # name varchar(255)
-    price = models.IntegerField()
-    description = models.TextField()
+    text = models.TextField(null=True, blank=False)
+    font_size = models.IntegerField()
+    theme = models.CharField(max_length=20, blank=False, choices=THEME)
+
