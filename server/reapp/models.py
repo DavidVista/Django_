aad5d819a9c16849d2dd3ae3ca5f3f8ec74ea1ef
DavidVista <ilyagrigorev0505@gmail.com>
2022-04-01 13:23:02 +0500

    updated Font, Text models, updated views, created templates

--- server/reapp/models.py
+++ server/reapp/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class Font(models.Model):
     TYPES = (
-        ("Regular", "Regular"),
-        ("Italic", "Italic"),
+        ("reg", "Regular"),
+        ("ital", "Italic"),
     )
     FAMILIES = (
         ('Vollkorn', 'Vollkorn'),
@@ -13,20 +14,21 @@ class Font(models.Model):
         ('Open Sans', 'Open Sans')
     )
     family = models.CharField(max_length=63, choices=FAMILIES)
-    weight = models.IntegerField(default=400, validators=[MaxValueValidator(900), MinValueValidator(400)])
+    weight = models.IntegerField(default=400, validators=[MaxValueValidator(600), MinValueValidator(400)])
     type = models.CharField(max_length=25, choices=TYPES)
 
-class Text(models.Model):
-    THEME = (
-    ('Light', 'Light'),
-    ('Dark', 'Dark'),
-    ('Monokai', 'Monokai'),
-    ('Chocolate', 'Chocolate')
-    )
 
+class Text(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    name = models.CharField(max_length=255) # name varchar(255)
+    name = models.CharField(max_length=255)  # name varchar(255)
     text = models.TextField(null=True, blank=True)
+    date_of_creation = models.DateField(null=True)
+    date_of_recent_change = models.DateField(null=True)
     font_size = models.IntegerField(default=16, validators=[MaxValueValidator(98), MinValueValidator(12)])
-    font = models.ManyToManyField(Font)
-    theme = models.CharField(max_length=20, choices=THEME)
+    font = models.ForeignKey(Font, on_delete=models.CASCADE, null=True)
+    THEME = (
+        ('while black', 'Light'),
+        ('black white', 'Dark'),
+        ('black red', 'Monokai')
+    )
+    theme_type = models.CharField(max_length=25, choices=THEME, null=True)
